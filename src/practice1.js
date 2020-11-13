e2e375c94161040512b4ed6e4c75648e1f1e95c8
xiaowen <846703636@qq.com>
2020-11-13 19:13:04 +0800

    finish homeworks

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (jsonData) => {
+  const { data, column } = jsonData;
+  var result = [];
+  data.forEach((item, currentIndex) => {
+    var dataObj = {};
+    column.forEach((e, index) => {
+      dataObj[e.name] = item[index];
+    });
+    result[currentIndex] = dataObj;
+  });
+  return result;
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,14 @@
-const inject
+const inject = (items, sections) => {
+  var result = [];
+  items.forEach((element, itemsIndex) => {
+    sections.forEach((item) => {
+      if (item.index === itemsIndex) {
+        result.push(item.content);
+      }
+    });
+
+    result.push(element);
+  });
+  return result;
+};
 export { inject };
